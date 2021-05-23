eeb9acbedb3a1381d55b956f004d71b2086517a5
Ken <kensikes988@gmail.com>
2021-05-22 20:08:01 -0700

    creating dropdown menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -15,3 +15,19 @@ function buildMetadata(sample) {
         })
       });
   }
+
+  //create dropdown
+function dropdown(){
+    var ID = d3.select("#selDataset");
+    d3.json("samples.json").then((data)=>{
+        var id = data.names;
+        id.forEach((sampleid)=>{
+            ID.append("option")
+            .text(sampleid)
+            .property("value",sampleid)
+        })
+        var sample1 = id[0];
+        buildMetadata(sample1);
+        buildCharts(sample1);
+    })
+}
